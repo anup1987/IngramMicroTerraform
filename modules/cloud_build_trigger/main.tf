ff838d36c750265353b2738349e4c390bde39e31
anup1987 <anupraj.aimit@gmail.com>
2022-05-25 15:43:42 +0800

    Update main.tf

--- modules/cloud_build_trigger/main.tf
+++ modules/cloud_build_trigger/main.tf
@@ -1,18 +1,17 @@
 resource "google_cloudbuild_trigger" "manual-trigger" {
   name        = var.name
-
+  for_each  = var.git_repo
+  
   source_to_build {
-    for_each  = var.git_repo
     uri       = each.value.uri
     ref       = each.value.branch
     repo_type = var.repo_type
   }
 
   git_file_source {
-    for_each  =  google_cloudbuild_trigger.manual-trigger
     path      = "cloudbuild.yaml"
-    uri       =  each.value.source_to_build.uri
-    revision  = each.value.source_to_build.ref
+    uri       =  each.value.uri
+    revision  =  each.value.branch
     repo_type = var.repo_type
   }
 
