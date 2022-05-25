bc39f59c24a0ffb3e4b27427fb6d6eada215fd77
anup1987 <anupraj.aimit@gmail.com>
2022-05-25 14:41:14 +0800

    Update variables.tf

--- stage/variables.tf
+++ stage/variables.tf
@@ -2,3 +2,8 @@ variable "name" {
   type        = string
   description = "The name of the Cloud Build resources"
 }
+
+variable "repo_type" {
+  type        = string
+  description = "The name of the Cloud Build resources"
+}
