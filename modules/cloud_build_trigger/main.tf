2101add1620539197700058578c8f80f065c8873
anup1987 <anupraj.aimit@gmail.com>
2022-05-25 14:11:04 +0800

    Update main.tf

--- modules/cloud_build_trigger/main.tf
+++ modules/cloud_build_trigger/main.tf
@@ -1,17 +1,4 @@
-resource "google_cloudbuild_trigger" "manual-trigger" {
-  name        = "manual-build-terraform"
-
-  source_to_build {
-    uri       = "https://github.com/anup1987/spring-boot-hello-world-1"
-    ref       = "refs/heads/master"
-    repo_type = "GITHUB"
-  }
-
-  git_file_source {
-    path      = "cloudbuild.yaml"
-    uri       = "https://github.com/anup1987/spring-boot-hello-world-1"
-    revision  = "refs/heads/master"
-    repo_type = "GITHUB"
-  }
-  
+module "build-trigger" {
+  source               = "../modules/cloud_build_trigger"
+  name                 = var.name
 }
