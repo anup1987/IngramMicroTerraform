dba58ad29b4775627ee5d25e8f1c19f20e810264
anup1987 <anupraj.aimit@gmail.com>
2022-05-25 13:05:05 +0800

    Create main.tf

---
+++ modules/cloud_build_trigger/main.tf
@@ -0,0 +1,17 @@
+resource "google_cloudbuild_trigger" "manual-trigger" {
+  name        = "manual-build-terraform"
+
+  source_to_build {
+    uri       = "https://github.com/anup1987/spring-boot-hello-world-1"
+    ref       = "refs/heads/master"
+    repo_type = "GITHUB"
+  }
+
+  git_file_source {
+    path      = "cloudbuild.yaml"
+    uri       = "https://github.com/anup1987/spring-boot-hello-world-1"
+    revision  = "refs/heads/master"
+    repo_type = "GITHUB"
+  }
+  
+}
