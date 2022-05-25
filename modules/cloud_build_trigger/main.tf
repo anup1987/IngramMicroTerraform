61b515e9a5a55bae720d0ff9113bc6f06ecdc1d1
anup1987 <anupraj.aimit@gmail.com>
2022-05-25 15:37:42 +0800

    Update main.tf

--- modules/cloud_build_trigger/main.tf
+++ modules/cloud_build_trigger/main.tf
@@ -2,15 +2,17 @@ resource "google_cloudbuild_trigger" "manual-trigger" {
   name        = var.name
 
   source_to_build {
-    uri       = "https://github.com/anup1987/spring-boot-hello-world-1"
-    ref       = "refs/heads/master"
+    for_each  = var.git_repo
+    uri       = each.value.uri
+    ref       = each.value.branch
     repo_type = var.repo_type
   }
 
   git_file_source {
+    for_each  =  google_cloudbuild_trigger.manual-trigger
     path      = "cloudbuild.yaml"
-    uri       = "https://github.com/anup1987/spring-boot-hello-world-1"
-    revision  = "refs/heads/master"
+    uri       =  each.value.source_to_build.uri
+    revision  = each.value.source_to_build.ref
     repo_type = var.repo_type
   }
 
