3cd6f639fd1d6804e8b490dff448c0113412d9b6
anup1987 <anupraj.aimit@gmail.com>
2022-05-28 01:04:36 +0800

    Update variables.tf

--- modules/cloud_build_trigger/variables.tf
+++ modules/cloud_build_trigger/variables.tf
@@ -5,27 +5,22 @@ variable "repo_type" {
   default = "GITHUB"
 }
 
-variable "git_repo" {
-  description = "Git Repository Details"
-  type = map
-  default = {
-      repo1 = {
-        name           = "app1"
-        uri            = "https://github.com/anup1987/spring-boot-hello-world-1"
-        branch         = "master"
-    },
- 
-    repo2 = {
-        name              = "app2"
-        uri               = "https://github.com/anup1987/cloud-build-samples/tree/main/maven-example"
-        branch            = "main"
-   }
-    
-   repo3 = {
-        name              = "app3"
-        uri               = "https://github.com/anup1987/helloworld"
-        branch            = "main"
-   }
-    
- }
+
+variable "name" {
+  type        = string
+  description = "The name of the Trigger"
 }
+
+variable "uri" {
+  type        = string
+  description = "The name of the URI"
+}
+
+variable "branch" {
+  type        = string
+  description = "The name of the Branch"
+}
+
+
+
+
