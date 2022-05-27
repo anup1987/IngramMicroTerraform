d2a0c00b693a7b596f9f934f5332f068ec5951a4
anup1987 <anupraj.aimit@gmail.com>
2022-05-28 02:01:16 +0800

    Update main.tf

--- stage/main.tf
+++ stage/main.tf
@@ -1,11 +1,10 @@
 module "build-trigger" {
   for_each  = var.git_repo
-  source               = "../modules/cloud_build_trigger"
+  source               = "../modules/cloud_build_run"
   name      = each.value.names
   uri       = each.value.uris
   branch    = "master"
   repo_type = var.repo_type
-  source           = "../modules/cloud_run"
   image_name       = var.image_name
 }
   
