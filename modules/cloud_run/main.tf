66fcdac44de75509af80a98711ee4b77082764d2
anup1987 <anupraj.aimit@gmail.com>
2022-05-28 00:25:34 +0800

    Update main.tf

--- modules/cloud_run/main.tf
+++ modules/cloud_run/main.tf
@@ -6,3 +6,18 @@ resource "google_project_service" "run" {
   service            = "run.googleapis.com"
   disable_on_destroy = false
 }
+
+resource "google_cloud_run_service" "app" {
+  
+   name     = "app"
+  
+  template {
+    spec {
+      containers {
+        image = data.google_container_registry_image.app.image_url
+      }
+    }
+  }
+  
+}
+  
