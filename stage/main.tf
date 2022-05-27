98e0e4bc4e92b940235caa7cc614ff98862994f7
anup1987 <anupraj.aimit@gmail.com>
2022-05-28 00:03:36 +0800

    Update main.tf

--- stage/main.tf
+++ stage/main.tf
@@ -1,3 +1,9 @@
 module "build-trigger" {
   source               = "../modules/cloud_build_trigger"
 }
+  
+module "cloud_run" {
+  source           = "../modules/cloud_run"
+  image_name       = var.image_name
+  depends_on       = [module.build-trigger]
+}
