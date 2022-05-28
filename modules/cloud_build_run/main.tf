a6fa1b4d24c56889be785b5dc5e3db967d1d8f99
anup1987 <anupraj.aimit@gmail.com>
2022-05-28 14:03:17 +0800

    Update main.tf

--- modules/cloud_build_run/main.tf
+++ modules/cloud_build_run/main.tf
@@ -1,5 +1,5 @@
 module "build-trigger_run" {
-  source               = "../module/cloud_build_trigger"
+  source               = "../modules/cloud_build_trigger"
   name      = var.name
   uri       = var.uri
   branch    = var.branch
