08f7ad7e36907b33aeb158d6b90bd7c63da571ec
anup1987 <anupraj.aimit@gmail.com>
2022-05-30 23:08:43 +0800

    Update main.tf

--- modules/cloud_build_trigger_run/main.tf
+++ modules/cloud_build_trigger_run/main.tf
@@ -8,7 +8,6 @@ module "build-trigger" {
   
 module "cloud_run" {
   source           = "../cloud_run"
-  name             = var.registry
   deploy_run_service_name = var.name
   depends_on       = [module.build-trigger]
 }
