05f06e8f317404e163c1a223e99a566671ab851d
anup1987 <anupraj.aimit@gmail.com>
2022-06-03 00:53:26 +0800

    Update main.tf

--- stage/run/main.tf
+++ stage/run/main.tf
@@ -1,6 +1,6 @@
 module "build-cloud_run" {
   for_each  = var.service_details
   source               = "../modules/cloud_run"
-  name      = each.value.name
+  serviceName      = each.value.name
   imagename = concat(each.value.registry,:,each.value.tag)
 }
