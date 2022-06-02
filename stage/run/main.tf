079588b0ad02145a1929afe1e99a077c94d97f8a
anup1987 <anupraj.aimit@gmail.com>
2022-06-03 00:39:53 +0800

    Update main.tf

--- stage/run/main.tf
+++ stage/run/main.tf
@@ -2,7 +2,5 @@ module "build-cloud_run" {
   for_each  = var.service_details
   source               = "../modules/cloud_run"
   name      = each.value.name
-  uri       = each.value.uri
-  branch    = "master"
-  repo_type = var.repo_type   
+  imagename = concat(each.value.registry,:,each.value.tag)
 }
