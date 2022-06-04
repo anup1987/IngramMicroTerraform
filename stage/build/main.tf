ba07f54aca6c4f360f00a1ce78dde23498f65104
anup1987 <anupraj.aimit@gmail.com>
2022-06-05 00:37:38 +0800

    Update main.tf

--- stage/build/main.tf
+++ stage/build/main.tf
@@ -7,7 +7,7 @@ terraform {
 
 module "build-trigger_and_run" {
   for_each  = var.git_repo
-  source               = "../modules/cloud_build_trigger"
+  source               = "../../modules/cloud_build_trigger"
   name      = each.value.name
   uri       = each.value.uri
   branch    = "master"
