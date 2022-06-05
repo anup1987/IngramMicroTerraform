6a739b2603fcaebbdac31b05f65891494b6e49c8
anup1987 <anupraj.aimit@gmail.com>
2022-06-05 23:04:46 +0800

    Update main.tf

--- stage/run/main.tf
+++ stage/run/main.tf
@@ -1,7 +1,6 @@
 
 module "build-cloud_run" {
-  for_each  = var.service_details
   source               = "../../modules/cloud_run"
-  serviceName      = each.value.serviceName
-  imagename = each.value.imagename
+  serviceName          = var.serviceName
+  imagename            = var.imagename
 }
